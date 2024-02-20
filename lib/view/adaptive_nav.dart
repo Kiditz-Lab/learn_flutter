8720bebcbac8694d7baf741c96226ed01189c046
Rifky Aditya Bastara <rifky@savantdegrees.com>
2024-02-20 22:55:23 +0700

    fix drawer

--- lib/view/adaptive_nav.dart
+++ lib/view/adaptive_nav.dart
@@ -1,18 +1,77 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:learn_flutter/components/change_theme_view.dart';
+import 'package:learn_flutter/components/responsive.dart';
 
-class AdaptiveNav extends StatefulWidget {
+class AdaptiveNav extends StatelessWidget {
   final Widget? child;
   const AdaptiveNav({super.key, this.child});
 
   @override
-  State<AdaptiveNav> createState() => _AdaptiveNavState();
+  Widget build(BuildContext context) {
+    return Responsive(
+      mobile: _MobileView(child: child),
+      desktop: _DesktopView(child: child),
+    );
+  }
 }
 
-class _AdaptiveNavState extends State<AdaptiveNav> {
-  int navIndex = 0;
+class _DesktopView extends StatelessWidget {
+  final Widget? child;
+  const _DesktopView({this.child});
 
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: false,
+        title: const Text('My App'),
+        actions: const [
+          ChangeThemeView(),
+        ],
+      ),
+      body: Row(
+        children: [
+          Drawer(
+            width: 250,
+            child: ListView(
+              children: const [
+                ListTile(
+                  leading: Icon(Icons.dashboard_outlined),
+                  title: Text('Dashboard'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.home_outlined),
+                  title: Text('Home'),
+                ),
+                ListTile(
+                  leading: Icon(Icons.person_2_outlined),
+                  title: Text('Profile'),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            child: child!,
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _MobileView extends StatefulWidget {
+  final Widget? child;
+  const _MobileView({
+    this.child,
+  });
+
+  @override
+  State<_MobileView> createState() => _MobileViewState();
+}
+
+class _MobileViewState extends State<_MobileView> {
+  int navIndex = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
